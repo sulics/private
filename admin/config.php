2b2a177637ad1d87c69c7b37fbe24c0b3371a7d9
sulics <lisucs@163.com>
2013-12-26 11:34:08 +0800

    init system

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.ocaddons.me/admin/');
+define('HTTP_CATALOG', 'http://www.ocaddons.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.ocaddons.me/admin/');
+define('HTTPS_CATALOG', 'http://www.ocaddons.me/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ocaddons/admin/');
+define('DIR_SYSTEM', '/var/www/ocaddons/system/');
+define('DIR_DATABASE', '/var/www/ocaddons/system/database/');
+define('DIR_LANGUAGE', '/var/www/ocaddons/admin/language/');
+define('DIR_TEMPLATE', '/var/www/ocaddons/admin/view/template/');
+define('DIR_CONFIG', '/var/www/ocaddons/system/config/');
+define('DIR_IMAGE', '/var/www/ocaddons/image/');
+define('DIR_CACHE', '/var/www/ocaddons/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/ocaddons/download/');
+define('DIR_LOGS', '/var/www/ocaddons/system/logs/');
+define('DIR_CATALOG', '/var/www/ocaddons/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '360weboy');
+define('DB_DATABASE', 'ocaddons');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.ocaddons.me/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.ocaddons.me/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ocaddons/catalog/');
+define('DIR_SYSTEM', '/var/www/ocaddons/system/');
+define('DIR_DATABASE', '/var/www/ocaddons/system/database/');
+define('DIR_LANGUAGE', '/var/www/ocaddons/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/ocaddons/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/ocaddons/system/config/');
+define('DIR_IMAGE', '/var/www/ocaddons/image/');
+define('DIR_CACHE', '/var/www/ocaddons/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/ocaddons/download/');
+define('DIR_LOGS', '/var/www/ocaddons/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '360weboy');
+define('DB_DATABASE', 'ocaddons');
+define('DB_PREFIX', 'oc_');
+?>
